7a27b367912deeec56d3cffe03d1a6c7a63e4202
PH - ACER <henriqueph@hotmail.com>
2020-10-30 11:23:45 -0300

    Updating portfolio. New projects and new experiences

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-$(function() {
-    var images = ["Burger.png", "RecipeHelper.png", "WeatherDashboard.png", "DayPlanner.png", "PasswordGenerator.png", "RecipeHelper2.png"];
+$(function () {
+    var images =
+        [
+            "Burger.png", "RecipeHelper.png", "WeatherDashboard.png", "DayPlanner.png",
+            "PasswordGenerator.png", "RecipeHelper2.png", "employee-directory.png", "fitness-tracker.png",
+            "budget-tracker.png", "quiz.png"
+        ];
     var nrImg = Math.floor(Math.random() * images.length);
     $("#img_portfolio").attr("src", "assets/img/" + images[nrImg]);
-    setInterval(function() {
+    setInterval(function () {
         if (++nrImg >= images.length) {
             nrImg = 0;
         }
